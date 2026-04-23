295ae9284af78f51b683e45bfe7f3ddaf1a912da
Xiangyin <1922483258@qq.com>
2026-04-23 13:55:30 +0800

    complete multi-line-comments rule architecture

---
+++ verible/verilog/tools/lint/testdata/multi_line_comments.sv
@@ -0,0 +1,12 @@
+// Test file for multi-line-comments rule
+// This file has multi-line comments without proper borders (violations expected)
+
+module multi_line_comments;
+
+  // This is a multi-line comment
+  // that spans multiple lines
+  // without proper border formatting
+
+  logic [7:0] data;
+
+endmodule
